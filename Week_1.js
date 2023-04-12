cfab8b22869e66ae5b7beae21c7f7097064ea3db
Rocco <Rocco@Divatos-MacBook-Air.local>
2023-04-12 18:00:52 -0400

    Week 1 Completed

---
+++ Week_1.js
@@ -0,0 +1,18 @@
+/*
+
+Write a function that takes the base and height of a triangle and return its area.
+
+triArea(3, 2) ➞ 3
+
+triArea(7, 4) ➞ 14
+
+triArea(10, 10) ➞ 50
+
+*/
+
+function triArea(x, y){
+    let area = x * y / 2;
+    return area;
+}
+
+console.log(triArea(10,10));
